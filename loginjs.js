e98a243b71f0742c6285a4362cb8337dd0c53008
juan10-coder <juan.henao.osorio@gmail.com>
2024-11-21 16:24:55 -0500

    cambio login

--- loginjs.js
+++ loginjs.js
@@ -1,49 +1,47 @@
 window.onload = () => {
     const button_ingresar = document.getElementById("button_ingresar");
-    const input_name = document.getElementById("input_name");
+    const input_email = document.getElementById("input_email");
     const input_password = document.getElementById("input_password");
     const label_message = document.getElementById("label_message");
 
-    input_name.setAttribute('value', sessionStorage.getItem('name'));
+    input_email.setAttribute('value', sessionStorage.getItem('email'));
     input_password.setAttribute('value', sessionStorage.getItem('password'));
 
     
     button_ingresar.addEventListener("click", async () => {
-        const name =   input_name.value;
+        const email =   input_email.value;
         const password = input_password.value;
 
-        sessionStorage.setItem('name', name);
+        sessionStorage.setItem('email', email);
         sessionStorage.setItem('password', password);
 
-        input_name.innerHTML = '';
+        input_email.innerHTML = '';
         input_password.innerHTML = '';
         
         const token = sessionStorage.getItem('token');
 
         if (!token || token == "undefined") {
-            await login(name, password);
+            await login(email, password);
         } 
     });
 }
 
 
-async function login(name, password) {
+async function login(email, password) {
     console.log("login");
     return fetch('http://127.0.0.1:8000/api/login', {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Authorization': 'Basic ' + btoa(`${email}:${password}`),
+            'Content-Type': 'application/json',
+            'User-Type': 'employee'
         },
-        body: JSON.stringify({
-            name: name,
-            password: password
-        })
     })
     .then(response => {
         if (response.ok) {
             return response.json().then(datos =>{
                 console.log(datos);
-                window.location.href = 'otro.html';
+                window.location.href = 'indexGestorPedidos.html';
             });
         } else {
             label_message.textContent = 'Nombre o contraseña incorrectos';
